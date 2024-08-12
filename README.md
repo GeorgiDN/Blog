03e933effab562296e5ae47116caf0801a8186ac
GeorgiDN <go6odn28@gmail.com>
2024-08-12 22:14:19 +0300

    delete past commits

---
+++ README.md
@@ -0,0 +1,6 @@
+# WebApp-Blog
+<<<<<<< HEAD
+## Blog wih django
+=======
+WebApp-Blog with Django
+>>>>>>> 77b01c83eac8b590960d30d024c8054b9e5c0dcf
---
+++ WebApp/blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from WebApp.blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_posted')
+    list_filter = ('date_posted', 'title')
+    search_fields = ('title', 'content', 'date_posted')
+    ordering = ('-date_posted',)
