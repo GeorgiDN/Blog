9c08fecff3c50908f207657e8b428281e0c2cc7b
GeorgiDN <go6odn28@gmail.com>
2024-08-15 22:03:07 +0300

    create profile

--- WebApp/users/models.py
+++ WebApp/users/models.py
@@ -1,1 +1,11 @@
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE,
+                                related_name='user_profile')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
