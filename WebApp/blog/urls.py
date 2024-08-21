7336b0299c6784ff92ab0c4132da384692852a1d
GeorgiDN <go6odn28@gmail.com>
2024-08-21 21:32:00 +0300

    create CreateView

--- WebApp/blog/urls.py
+++ WebApp/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from WebApp.blog.views import PostListView, PostDetailView
+from WebApp.blog.views import PostListView, PostDetailView, PostCreateView
 from WebApp.blog import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
