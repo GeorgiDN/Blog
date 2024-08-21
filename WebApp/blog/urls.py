1a57659e8d7427df5fd38d5feadf70a44c6d460a
GeorgiDN <go6odn28@gmail.com>
2024-08-21 22:14:00 +0300

    create UpdateView

--- WebApp/blog/urls.py
+++ WebApp/blog/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from WebApp.blog.views import PostListView, PostDetailView, PostCreateView
+from WebApp.blog.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+)
 from WebApp.blog import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about'),
 ]
