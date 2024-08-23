28b4a3fa5780df9862149d80364c6067d721a9d4
GeorgiDN <go6odn28@gmail.com>
2024-08-24 00:20:32 +0300

    create post_details template

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -35,7 +35,7 @@ class UserPostListView(ListView):
 
     paginate_by = 5
 
-    def get_query_set(self):
+    def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
 
