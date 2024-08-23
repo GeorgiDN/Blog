c55fced61646947113c6f34f5becd7b4cd85cc43
GeorgiDN <go6odn28@gmail.com>
2024-08-23 23:31:15 +0300

    create UserPostListView

--- WebApp/blog/urls.py
+++ WebApp/blog/urls.py
@@ -5,11 +5,13 @@ from WebApp.blog.views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 from WebApp.blog import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
